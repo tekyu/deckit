bb9db267fd2816b3ff7dbe0e23f42924bb6211b5
Patryk <patryyk21@gmail.com>
2018-11-06 21:14:34 +0100

    migrating to CRA 2.0

--- client/src/index.js
+++ client/src/index.js
@@ -1,24 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter } from "react-router-dom";
-import "./css/reset.css";
-import "./index.css";
-import App from "./App";
-// import registerServiceWorker from "./registerServiceWorker";
-import reducer from "./store/reducer";
-import thunk from "redux-thunk";
-const store = createStore(reducer, applyMiddleware(thunk));
-const app = (
-    <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>
-);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-window.setTimeout(() => {
-    ReactDOM.render(app, document.getElementById("root"));
-}, 500);
-// registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
