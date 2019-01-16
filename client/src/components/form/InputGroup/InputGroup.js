4f5569f89a4d12629ca48ac0972c4c2f2b194213
Patryk <patryyk21@gmail.com>
2019-01-16 22:07:55 +0100

    login and register components

---
+++ client/src/components/form/InputGroup/InputGroup.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+const InputGroup = ({
+	name,
+	labelText,
+	type,
+	placeholder,
+	id,
+	classes = [],
+	handler,
+	...props
+}) => {
+	const inputClass = ["inputGroupInput", ...classes].join(", ");
+
+	return (
+		<div className="inputGroup">
+			<label htmlFor={name}>{labelText}</label>
+			<input
+				name={name}
+				type={type}
+				{...(id ? (id = { id }) : "")}
+				className={inputClass}
+				placeholder={placeholder ? placeholder : ""}
+				onChange={handler}
+			/>
+		</div>
+	);
+};
+
+export default InputGroup;
--- server/src/utils/memory-usage.js
+++ server/src/utils/memory-usage.js
@@ -1,22 +1,34 @@
 var memory_usage = {
-    unit: ['', 'K', 'M', 'G', 'T', 'P'],
-    
-    bytesToSize: function(input, precision) {
-        var index = Math.floor(Math.log(input) / Math.log(1024));
-        if (this.unit >= this.unit.length) return input + ' B';
-        return (input / Math.pow(1024, index)).toFixed(precision) + ' ' + this.unit[index] + 'B'            
-    },
-    
-    testUsage: function() {
-        const _this = this;
-        const usage  = process.memoryUsage(); 
-        console.log('==================================');
-        console.log('  Empty Node Module Memory Usage  ');
-        console.log('==================================');
-        
-        setInterval(function() {
-            console.log('RSS: ' + _this.bytesToSize(usage.rss, 3), 'and Heap:', _this.bytesToSize(usage.heapUsed, 3), 'of', _this.bytesToSize(usage.heapTotal, 3), 'total');
-        }, 1000);        
-    }
-}
-module.exports =  memory_usage;
+	unit: ["", "K", "M", "G", "T", "P"],
+
+	bytesToSize: function(input, precision) {
+		var index = Math.floor(Math.log(input) / Math.log(1024));
+		if (this.unit >= this.unit.length) return input + " B";
+		return (
+			(input / Math.pow(1024, index)).toFixed(precision) +
+			" " +
+			this.unit[index] +
+			"B"
+		);
+	},
+
+	testUsage: function() {
+		const _this = this;
+		const usage = process.memoryUsage();
+		console.log("==================================");
+		console.log("  Empty Node Module Memory Usage  ");
+		console.log("==================================");
+
+		setInterval(function() {
+			console.log(
+				"RSS: " + _this.bytesToSize(usage.rss, 3),
+				"and Heap:",
+				_this.bytesToSize(usage.heapUsed, 3),
+				"of",
+				_this.bytesToSize(usage.heapTotal, 3),
+				"total"
+			);
+		}, 1000);
+	}
+};
+module.exports = memory_usage;
