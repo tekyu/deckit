177c952fabd18e1631a513d767f2a447afc68349
Patryk <patryyk21@gmail.com>
2019-07-19 19:09:50 +0200

    [Back] Adding typescript and making initial refactor

--- server/src/utils/memory-usage.ts
+++ server/src/utils/memory-usage.ts
@@ -1,8 +1,10 @@
-var memory_usage = {
-	unit: ["", "K", "M", "G", "T", "P"],
-
-	bytesToSize: function(input, precision) {
-		var index = Math.floor(Math.log(input) / Math.log(1024));
+export class MemoryUsage {
+	unit: Array<string>;
+	constructor() {
+		this.unit = ["", "K", "M", "G", "T", "P"];
+	}
+	bytesToSize(this: MemoryUsage, input: any, precision: number) {
+		const index = Math.floor(Math.log(input) / Math.log(1024));
 		if (this.unit >= this.unit.length) return input + " B";
 		return (
 			(input / Math.pow(1024, index)).toFixed(precision) +
@@ -10,25 +12,22 @@ var memory_usage = {
 			this.unit[index] +
 			"B"
 		);
-	},
-
-	testUsage: function() {
-		const _this = this;
+	}
+	testUsage(this: MemoryUsage) {
 		const usage = process.memoryUsage();
 		console.log("==================================");
 		console.log("  Empty Node Module Memory Usage  ");
 		console.log("==================================");
 
-		setInterval(function() {
+		setInterval(() => {
 			console.log(
-				"RSS: " + _this.bytesToSize(usage.rss, 3),
+				"RSS: " + this.bytesToSize(usage.rss, 3),
 				"and Heap:",
-				_this.bytesToSize(usage.heapUsed, 3),
+				this.bytesToSize(usage.heapUsed, 3),
 				"of",
-				_this.bytesToSize(usage.heapTotal, 3),
+				this.bytesToSize(usage.heapTotal, 3),
 				"total"
 			);
 		}, 1000);
 	}
-};
-module.exports = memory_usage;
+}
