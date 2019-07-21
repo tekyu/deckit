db97faa049e34f4864c96e89f0170e2e5fedbba8
Nerond32 <nerond32@gmail.com>
2019-07-21 02:27:52 +0200

    Added proptypes to project, App refactor to functional component

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,40 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import ModalContainer from 'modals/ModalContainer';
 import Header from './Header';
 import Content from './Content';
-// import axios from "@app/axios";
 
-class App extends Component {
-  state = {
-    auth: false
-  };
+const App = ({ auth, modalType, showModal }) => {
+  return (
+    <React.Fragment>
+      <Header />
+      <Content auth={auth} />
+      {showModal && <ModalContainer type={modalType} />}
+    </React.Fragment>
+  );
+};
 
-  render() {
-    return (
-      <React.Fragment>
-        <Header />
-        <Content auth={this.state.auth} />
-        {this.props.showModal ? (
-          <ModalContainer type={this.props.modalType} />
-        ) : null}
-      </React.Fragment>
-    );
-  }
-}
+App.propTypes = {
+  auth: PropTypes.bool.isRequired,
+  modalType: PropTypes.string,
+  showModal: PropTypes.bool.isRequired
+};
 
 const mapStateToProps = state => {
   return {
     auth: state.auth,
-    showModal: state.showModal,
-    modalType: state.modalType
+    modalType: state.modalType,
+    showModal: state.showModal
   };
 };
-const mapDispatchToProps = dispatch => {
-  return {};
-};
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
+export default connect(mapStateToProps)(App);
