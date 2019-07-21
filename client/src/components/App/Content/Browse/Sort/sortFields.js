1ccf43dc64c0aeed9c68bd9fb44f50b0305bfcff
Nerond32 <nerond32@gmail.com>
2019-07-21 17:36:18 +0200

    Refactored Sort and Select to work nicely together

---
+++ client/src/components/App/Content/Browse/Sort/sortFields.js
@@ -0,0 +1,20 @@
+const sortData = [
+  {
+    fieldName: 'Author',
+    searchable: true
+  },
+  {
+    fieldName: 'Latest',
+    searchable: false
+  },
+  {
+    fieldName: 'Name',
+    searchable: true
+  },
+  {
+    fieldName: 'Players',
+    searchable: false
+  }
+];
+
+export default sortData;
