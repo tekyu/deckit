0523f45e9d72f0e3841660f8b54652a4767aa6ed
Nerond32 <nerond32@gmail.com>
2019-07-21 02:34:48 +0200

    Header simplified to functional component

--- client/src/components/App/Header/Header.jsx
+++ client/src/components/App/Header/Header.jsx
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Logo from 'components/Logo/Logo';
 import Navigation from './Navigation';
 import * as styles from './Header.module.scss';
 import AccountBox from './AccountBox';
 
-class Header extends Component {
-  render() {
-    return (
-      <header className={styles.header}>
-        <Logo />
-        <Navigation />
-        <AccountBox />
-      </header>
-    );
-  }
-}
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <Logo />
+      <Navigation />
+      <AccountBox />
+    </header>
+  );
+};
 
 export default Header;
